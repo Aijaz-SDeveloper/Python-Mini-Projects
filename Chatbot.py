2b4a0d327b27a9e052a8b1e207443457c7d5046b
Aijaz Ali <aijazbcs@gmail.com>
2026-09-08 11:51:06 +0500

    Add Chatbot AI

---
+++ Chatbot.py
@@ -0,0 +1,32 @@
+#Rule Based AI Python Chatbot
+import datetime
+import time
+from DataDictionary import responses
+name = input("Please enter your name: ")
+present_time = datetime.datetime.now().hour
+
+if present_time < 11:
+    print(f"Good Morning {name}!")
+
+elif present_time < 17:
+    print(f"Good Afternoon {name}!")
+elif present_time < 20:
+    print(f"Good Evening {name}!")
+else:
+    print(f"Good Night {name}!")
+
+print("Hi! Welcome to Rule Based Chatbot")
+print("You can ask me basic question, Type 'Bye' to exit from the chat bot")
+
+#Chatbot Memory - Dictionary
+while True:
+    #Take user input
+    userinput = input("Please ask me your question: ").lower().strip()
+
+    if userinput in responses:
+        print(responses[userinput])
+    else:
+        print("I'm sorry, I don't understand that.")
+
+    if userinput == "exit":
+        break
